bd3635db9d04e51410852972fd1ba18c7a7dbe64
Tyler Maier <tyler_maier@yahoo.com>
2016-09-02 11:12:00 -0600

    add getAll and getOne contact(s)

--- src/server/routes/contacts.js
+++ src/server/routes/contacts.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../lib/data');
 
 // const indexController = require('../controllers/index');
 
-router.get('/contacts', (req, res, next) => {
+router.get('/contacts', getAll);
+router.get('/contacts/:id', getOne);
+
+function  getAll (req, res, next) {
   const renderObject = {};
   renderObject.title = 'Contact List';
+  renderObject.table = data.all;
   res.render('contacts', renderObject);
-});
+}
+
+function  getOne (req, res, next) {
+  const contactID = req.params.id;
+  const renderObject = {};
+
+
+  renderObject.title = 'Contact List';
+  renderObject.contact = data.all[contactID];
+
+  if (contactID < data.all.length) {
+    res.render('contacts', renderObject);
+  } else {
+    res.send('No Contact');
+  }
+
+}
 
 module.exports = router;
